2ff440280b2d0026846314c3d6dd5b1420674e78
Othman Taik <taikothman1997@gmail.com>
2020-11-17 16:40:02 +0100

    Completed

--- js/vue.js
+++ js/vue.js
@@ -1,26 +1,26 @@
 const sliderApp = new Vue({
   el: '#root',
   data: {
-   images: ["images/image1.jpg","images/image2.jpg","images/image3.jpg","images/image4.jpg"],
-   currentIndex: 0
+   img: ["images/image1.jpg","images/image2.jpg","images/image3.jpg","images/image4.jpg"],
+   currentIndex: 0,
   },
 
   methods: {
     prev: function() {
       if (this.currentIndex === 0) {
-        this.currentIndex = images.length - 1;
+        this.currentIndex = img.length - 1;
       } else {
          this.currentIndex--;
       }
     },
 
     next: function() {
-       if (this.currentIndex === images.length - 1) {
+       if (this.currentIndex === img.length - 1) {
           this.currentIndex = 0;
        } else {
           this.currentIndex++;
        }
-    }
-  }
+    },
+  },
 
 });
